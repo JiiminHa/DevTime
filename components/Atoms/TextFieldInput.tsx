ca4a0b14dbf95d23239397170bc08fdf91a25a6c
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-12-06 04:35:39 +0900

    feat: 입력/체크 박스 Atoms 개선

---
+++ components/Atoms/TextFieldInput.tsx
@@ -0,0 +1,38 @@
+'use client';
+interface TextfieldProps {
+  placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  className?: string;
+  error?: string;
+}
+
+export const TextfieldInput = ({
+  placeholder = '',
+  value,
+  onChange,
+  className = 'bg-gray-50 text-black',
+}: TextfieldProps) => {
+  const inputProps =
+    value !== undefined
+      ? {
+          value,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+            onChange?.(e.target.value),
+        }
+      : {
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+            onChange?.(e.target.value),
+        };
+
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <input
+        type='text'
+        placeholder={placeholder}
+        {...inputProps}
+        className={`text-body rounded-sm bg-gray-50 px-4 py-3`}
+      />
+    </div>
+  );
+};
