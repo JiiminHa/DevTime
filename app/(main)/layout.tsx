9fecbbbb9856fbaeedbf62d151c7411369260403
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-12-18 12:25:48 +0900

    feat: Navbar 추가 및 메인 레이아웃 적용

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,13 @@
+import {Navbar} from '@/src/shared/ui/navbar';
 import type {ReactNode} from 'react';
 
 export default function MainLayout({children}: {children: ReactNode}) {
-  return <>{children}</>;
+  return (
+    <div className='relative min-h-screen w-full overflow-hidden bg-linear-to-b from-neutral-100 to-slate-200'>
+      <div className='flex justify-center pt-4'>
+        <Navbar />
+      </div>
+      {children}
+    </div>
+  );
 }
